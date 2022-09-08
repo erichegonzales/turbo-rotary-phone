24243d42f5c11095e764c24a9add1bc08f03fe63
Eriche Gonzales <erichelouise@gmail.com>
2022-09-08 11:53:57 -0400

    done with challenge

--- app/serializers/vendor_with_sweets_serializer.rb
+++ app/serializers/vendor_with_sweets_serializer.rb
@@ -1,4 +1,5 @@
 class VendorWithSweetsSerializer < ActiveModel::Serializer
-  attributes :id, :vendor_id, :sweet_id, :price
+  attributes :id, :vendor_id, :sweet_id, :name, :price
+  has_many :sweets
   has_many :vendor_sweets
 end
